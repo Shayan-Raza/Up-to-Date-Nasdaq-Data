ad9e5b3eb992e6bab040c0105f001090a1b1da44
Shayan Raza <shayanraza07@gmail.com>
2022-12-15 22:23:31 +0500

    scheduling to run everyday

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 import pandas as pd 
 import yfinance as yf
 from datetime import date
+import schedule
+import time
 
 def daily_data() : 
     #Getting Ticker symbols from https://www.nasdaq.com/market-activity/quotes/nasdaq-ndx-index
@@ -9,10 +11,12 @@ def daily_data() :
     nasdaq_symbols = list(nasdaq_symbols.values) #Converting symbols to a list
 
     #Using yfinance to get data from the tickers
-    data = yf.download(tickers=nasdaq_symbols)
+    data = yf.download(tickers=nasdaq_symbols, group_by="tickers" )
     today = date.today()
     data.to_csv(f"results/nasdaq_{today}.csv")
-    
 
-
-daily_data()
+#Scheduling function to run everyday
+schedule.every().day.at("01:00").do(daily_data,"Data is here")
+while True: 
+    schedule.run_pending()
+    time.wait(60)
